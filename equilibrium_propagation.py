a8d4c7e1c54d18ffdcaeead17fdf03c0c761f329
amccaugh <amccaugh@gmail.com>
2019-01-04 10:59:26 -0700

    Creating tests to check E and R(s) and clipping explicitly

--- equilibrium_propagation.py
+++ equilibrium_propagation.py
@@ -19,7 +19,7 @@ W = np.zeros([num_neurons,num_neurons])
 for n in range(len(layer_sizes)-1): # Make weights only exist from one layer to the next
     wll = np.random.randn(layer_sizes[n+1],layer_sizes[n]) # The weight matrix for one layer to the next
     wll *= np.sqrt(2/(layer_sizes[n]))     # Glorot-Bengio (Xavier) weight normalization
-#    wll *= 0.1
+#    wll *= 0.2
     i = layer_indices[n+1]
     j = layer_indices[n]
     di = wll.shape[0]
@@ -42,21 +42,45 @@ ihy = list(range(layer_indices[1], layer_indices[-1]))
 def rho(s):
     return np.clip(s,0,1)
 
+def rhoprime(s):
+    rp = s*0
+    rp[(0<s) & (s<1)] = 1
+    return rp
+
 def E(s, W):
-    term1 = 0.5*s.T*s
-    term2 = -0.5 * rho(s).T @ W @ rho(s)
+    term1 = sum(0.5*s.T*s)
+    term2 = sum(-0.5 * rho(s).T @ W @ rho(s))
+#    term3 = -np.sum([b[i]*rho(s[i]) for i in range(len(b))])
+    return term1 + term2 # + term3
+
+def E_test(s, W): # Written out explicitly to check form of E(s,W)
+    term1 = 0
+    term2 = 0
+    for i in range(W.shape[0]):
+        term1 += 0.5*s[i]**2
+        for j in range(W.shape[1]):
+            if i == j:
+                term2 += 0
+            else:
+                term2 += -0.5*W[i,j]*rho(s[i])*rho(s[j])
 #    term3 = -np.sum([b[i]*rho(s[i]) for i in range(len(b))])
     return sum(term1 + term2) # + term3
 
-
-eps = 0.01
+#def Rs_test(s, W): # Written out explicitly to check form of E(s,W)
+Rs = np.zeros(np.array(s).shape)
+for i in range(W.shape[0]):
+    for j in range(W.shape[1]):
+        Rs[i] += sum(W[i,j]*rho(s[j]))
+    
+    
+eps = 1e-3
 # Compute free-phase fixed point
 states = []
 energies = []
-for n in range(1000):
+for n in range(10000):
     mu = np.matmul(W,rho(s)) - s
     s[ihy] += eps*mu[ihy]
-#    s[ihy] = np.clip(s[ihy], 0, 1) # Clip state
+    s[ihy] = np.clip(s[ihy], 0, 1) # Clip state; equivalent to multiplying R(s) by rhoprime(s)
     states.append(np.array(s).flatten().tolist())
     energies.append(E(s,W))
 
