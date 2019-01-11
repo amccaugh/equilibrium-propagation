e15bcd4344f9ea69f45083ad9c97d34d010687fc
amccaugh <amccaugh@gmail.com>
2019-01-10 18:22:25 -0700

    WIP torch conversion

--- equilibrium_propagation.py
+++ equilibrium_propagation.py
@@ -1,6 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from numba import jit
+import torch
+
+device = torch.device("cpu")
+torch.set_default_dtype(torch.float)
+dtype = torch.float
 
 # TODO / things to try:
 # - Divide weight matrix by 10
@@ -9,8 +13,9 @@ from numba import jit
 # Implement randomize_beta (beta gets chosen from randn(1) and see if it helps
 
 #%% Neuron implementation
-np.random.seed(seed = 0)
-
+# Following pytorch example from https://pytorch.org/tutorials/beginner/pytorch_with_examples.html
+# Helpful short and to-the-point torch tutorial: https://jhui.github.io/2018/02/09/PyTorch-Basic-operations/
+torch.manual_seed(seed = 0)
 
 layer_sizes = [3, 20, 20, 4]
 layer_indices = np.cumsum([0] + layer_sizes)
@@ -23,6 +28,7 @@ ih = slice(layer_indices[1], layer_indices[-2])
 ihy = slice(layer_indices[1], layer_indices[-1])
 
 def intialize_weight_matrix(layer_sizes, seed = None):
+    np.random.seed(seed = 0)
     W = np.zeros([num_neurons,num_neurons])
     W_mask = np.zeros(W.shape)
     # Initialize weights matrix
@@ -38,30 +44,44 @@ def intialize_weight_matrix(layer_sizes, seed = None):
         W_mask[i:i+di, j:j+dj] = wll*0 + 1
     W += W.T # Make weights symmetric
     W_mask += W_mask.T
-#    W = np.matrix(W)
-#    W_mask = np.matrix(W_mask)
+    W = torch.from_numpy(W).float() # Convert to float Tensor
+    W_mask = torch.from_numpy(W_mask).byte() # .byte() is the closest thing to a boolean tensor pytorch has
+     # Line up dimensions so that the zeroth dimension is the batch #
+    W = W.unsqueeze(0)
+    W_mask = W_mask.unsqueeze(0)
     return W, W_mask
 
 def random_initial_state(batch_size = 7, seed = None):
-    np.random.seed(seed = seed)
-    s = np.random.rand(batch_size, num_neurons)
+    torch.manual_seed(seed = seed)
+    s = torch.rand(batch_size, num_neurons)
     return s
 
 def rho(s):
-    return np.clip(s,0,1)
+    return torch.clamp(s,0,1)
 
 def rhoprime(s):
-    rp = s*0
+    rp = torch.zeros()
     rp[(0<=s) & (s<=1)] = 1 # SUPER IMPORTANT! if (0<s) & (s<1) zeros + ones cannot escape
     return rp
 
+#def E(s, W):
+#    term1 = 0.5*np.sum(np.multiply(s,s),axis = 1)
+#    term2 = -0.5 * np.sum(np.multiply(rho(s).dot(W),rho(s)),axis = 1)
+##    term3 = -np.sum([b[i]*rho(s[i]) for i in range(len(b))])
+#    return term1 + term2 # + term3
+
 def E(s, W):
-    term1 = 0.5*np.sum(np.multiply(s,s),axis = 1)
-    term2 = -0.5 * np.sum(np.multiply(rho(s).dot(W),rho(s)),axis = 1)
+    term1 = 0.5*torch.sum(s*s, dim = 1)
+    rho_s = rho(s)
+    term2 = torch.matmul(rho_s.unsqueeze(2), rho_s.unsqueeze(1))
+    term2 *= W
+    term2 = -0.5 * torch.sum(term2, dim = [1,2])
 #    term3 = -np.sum([b[i]*rho(s[i]) for i in range(len(b))])
     return term1 + term2 # + term3
-
-
+#
+#def C(y, d):
+#    return 0.5*np.linalg.norm(y-d, axis = 1)**2
+    
 def C(y, d):
     return 0.5*np.linalg.norm(y-d, axis = 1)**2
 
@@ -135,7 +155,7 @@ def weight_update(W, W_mask, beta, s_free_phase, s_clamped_phase):
 #def weight_update_explicit(W, W_mask, beta, s_free_phase, s_clamped_phase):
 #    # W_mask = matrix of shape(W) with 1s or zeros based on 
 #    # whether the connection / weight between i and j exists
-#    dW = np.zeros((batch_size,) + W.shape)
+#    dW = torch.zeros((batch_size,) + W.shape)
 #    rs = rho(s_free_phase)
 #    rc = rho(s_clamped_phase)
 #    for n in range(batch_size):
@@ -154,17 +174,25 @@ def update_weights(W, beta, s_free_phase, s_clamped_phase, learning_rate = 1):
 def target_matrix(seed = None):
     """ Generates a target of the form y = Tx
     """
-    np.random.seed(seed = seed)
-    T = np.random.rand(layer_sizes[-1], layer_sizes[0])/5
+    torch.manual_seed(seed = seed)
+    T = torch.rand((1, layer_sizes[-1], layer_sizes[0]), dtype = dtype)/5
     return T
     
+    
+#def generate_targets_old(s, T):
+#    """ Creates `d`, the target to which `y` will be weakly-clamped
+#    """
+##    d = np.matmul(T,s[:,iy])
+#    x = s[:,ix]
+#    d = np.einsum('jk,ik->ij', T, x)
+#    return d
     
 def generate_targets(s, T):
     """ Creates `d`, the target to which `y` will be weakly-clamped
     """
 #    d = np.matmul(T,s[:,iy])
-    x = s[:,ix]
-    d = np.einsum('jk,ik->ij', T, x)
+    x = s[:,ix].unsqueeze(2)
+    d = torch.matmul(T,x).squeeze()
     return d
 
 
